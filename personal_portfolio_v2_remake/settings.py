79288033484b4fbb5ac08dd7449408d172f96315
Juicyyyyyyy <declan.lowee@gmail.com>
2024-02-08 18:21:04 +0100

    Update settings.py

--- personal_portfolio_v2_remake/settings.py
+++ personal_portfolio_v2_remake/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['corentin-dupaigne.herokuapp.com, corentindupaigne.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['corentin-dupaigne.herokuapp.com, corentindupaigne.herokuapp.com', '127.0.0.1:8000', 'corentin-dupaigne-1f8550da0418.herokuapp.com']
 
 
 # Application definition
